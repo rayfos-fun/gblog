52e6897c3ad3c928b60b4e304278ea81d9b02ac7
Rayfos <rayfos.fun@gmail.com>
2025-07-26 20:15:26 -0700

    update zh-tw/rock-paper-scissors

---
+++ docs/zh-tw/rock-paper-scissors.js
@@ -0,0 +1,22 @@
+const choices = ["rock", "paper", "scissors"];
+const resultElement = document.getElementById("result");
+
+function playRound(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = `對手出${computerChoice}，雙方平手!`;
+  } else {
+    result = `玩家出${playerChoice}，對手出${computerChoice}.`
+    if ((playerChoice === "石頭" && computerChoice === "剪刀") ||
+        (playerChoice === "布" && computerChoice === "石頭") ||
+        (playerChoice === "剪刀" && computerChoice === "布")) {
+      result += '玩家';
+    } else {
+      result += '對手';
+    }
+    result += '勝利！';
+  }
+  resultElement.textContent = result;
+}
