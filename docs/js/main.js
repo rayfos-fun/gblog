8b90f96e1e83195f7c9cae453888d5d546d2a0a2
Rayfos <rayfos.fun@gmail.com>
2025-08-02 14:40:41 -0700

    fetch(url) only if querySelector finds something

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,19 @@
 loadHtml = function(selector, url) {
-  fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('HTTP error ' + response.status);
-    }
-    return response.text();
-  })
-  .then(html => {
-    document.querySelector(selector).innerHTML = html;
-  })
-  .catch(error => {
-    console.error('Load HTML failure:', error);
-  });
+  const element = document.querySelector(selector);
+  if (element) {
+    fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+      }
+      return response.text();
+    })
+    .then(html => {
+      element.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Load HTML failure:', error);
+    });
+  }
 }
 loadHtml('#top-sitemap-placeholder', 'sitemap.html');
