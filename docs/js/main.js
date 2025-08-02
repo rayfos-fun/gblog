f7c92c931d2abfda0baab8fdb64ae1a4aecf797c
Rayfos <rayfos.fun@gmail.com>
2025-08-02 14:48:46 -0700

    responsive design for font-family

--- docs/js/main.js
+++ docs/js/main.js
@@ -17,3 +17,13 @@ loadHtml = function(selector, url) {
   }
 }
 loadHtml('#top-sitemap-placeholder', 'sitemap.html');
+
+const path = window.location.pathname;
+const body = document.body;
+if (path.includes('/zh-tw/')) {
+  body.classList.add('lang-zh-tw');
+} else if (path.includes('/zh-cn/')) {
+  body.classList.add('lang-zh-cn');
+} else {
+  body.classList.add('lang-en');
+}
