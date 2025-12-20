f9794d4576822f0efa3094894c9f23b5a1e5b1f1
Rayfos <rayfos.fun@gmail.com>
2025-12-19 22:54:54 -0800

    add header Content-Type for sitemap.url

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, send_from_directory
+from flask import Flask, redirect, make_response, send_from_directory
 
 import logging
 import os
@@ -16,16 +16,27 @@ def redirect_old_gblog(subpath=''):
 @app.route('/', defaults={'url': ''})
 @app.route('/<path:url>')
 def serve_index(url):
-  logging.info(f'!serve_index(url={url})')
-  filepath = os.path.join(app.static_folder, url)
-  if os.path.isfile(filepath):
-    return send_from_directory(app.static_folder, url)
-  elif os.path.isdir(filepath):
-    index_filepath = os.path.join(filepath, 'index.html')
-    if os.path.isfile(index_filepath):
-      return send_from_directory(filepath, 'index.html')
-  return send_from_directory(app.static_folder, '404.html'), 404
+  target_file = '404.html'
+  status_code = 404
 
+  full_path = os.path.join(app.static_folder, url)
+  if os.path.isfile(full_path):
+    target_file = url
+    status_code = 200
+  elif os.path.isdir(full_path):
+    index_path = os.path.join(full_path, 'index.html')
+    if os.path.isfile(index_path):
+      target_file = os.path.join(url, 'index.html')
+      status_code = 200
+
+  response = make_response(send_from_directory(app.static_folder, target_file))
+  response.status_code = status_code
+
+  if target_file.endswith('.xml'):
+    response.headers['Content-Type'] = 'application/xml; charset=utf-8'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+
+  return response
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
