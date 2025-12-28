51c2b94a08ddcb91e92219725d3a37df96c27181
Rayfos <rayfos.fun@gmail.com>
2025-12-28 12:04:07 -0800

    use secret manager to manage google login

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, make_response, request, send_from_directory
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, redirect, make_response, request, send_from_directory, session, url_for
+from google.cloud import secretmanager
 
 import logging
 import os
@@ -7,6 +9,57 @@ STATIC_FOLDER = 'jekyll-site/_site'
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 logging.basicConfig(level=logging.INFO)
+
+def get_google_secrets():
+  project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
+  secret_id = 'RAYFOS_SECRET'
+  version_id = "latest"
+  client = secretmanager.SecretManagerServiceClient()
+  response = client.access_secret_version(request={"name": f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"})
+  return json.loads(response.payload.data.decode("UTF-8"))
+
+if os.environ.get('GAE_ENV', '').startswith('standard'):
+  print("Loading config from Google Secret Manager...")
+  try:
+    config = get_google_secrets()
+    app.secret_key = config.get('SECRET_KEY')
+    GOOGLE_CLIENT_ID = config.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = config.get('GOOGLE_CLIENT_SECRET')
+  except Exception as e:
+    print(f"Failed to load secrets from GSM: {e}")
+    raise e
+else:
+  print("💻 Loading config from Local .env file...")
+  from dotenv import load_dotenv
+  load_dotenv()
+  app.secret_key = os.environ.get('SECRET_KEY')
+  GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+  GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+
+oauth = OAuth(app)
+google = oauth.register(
+  name='google',
+  client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+  client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
+  server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+  client_kwargs={'scope': 'openid email profile'}
+)
+
+@app.route('/auth/callback')
+def auth():
+  token = google.authorize_access_token()
+  user_info = token.get('userinfo')
+  session['user'] = user_info
+  return redirect('https://rayfos.fun/?login=success')
+
+@app.route('/login')
+def login():
+  return google.authorize_redirect(url_for('auth', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('https://rayfos.fun')
 
 @app.route('/gblog/')
 @app.route('/gblog/<path:subpath>')
@@ -19,13 +72,13 @@ def redirect_old_gblog(subpath=''):
 def serve_index(url):
   target_langs = ['en', 'zh-cn', 'zh-tw']
   if any(url.startswith(lang + '/') or url == lang for lang in target_langs):
-    # /LANG/*
+    # /$LANG/*
     pass
   elif os.path.isfile(os.path.join(app.static_folder, url)):
-    # isfile e.g. robots.txt, sitemap.xml, ...et.
+    # isfile($URL) e.g. robots.txt, sitemap.xml, ...etc.
     pass
-  else:  # not /LANG/* and not isfile
-    # => redirect * to /LANG/*
+  else:  # not /$LANG/* and not isfile($URL)
+    # => redirect $URL to /$LANG/$URL
     best = request.accept_languages.best_match(['zh-CN', 'zh-TW', 'en'])
     if best:
       prefix = best.lower()
