26f80f2d38c2757309efbdc24bf6a94e124e7d67
Rayfos <rayfos.fun@gmail.com>
2026-01-10 00:48:55 -0800

    npx astro add tailwind

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,16 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rayfos.fun',
   base: '/',
+
   build: {
     // $URL/index.html
     format: 'directory',
   },
+
   server: {
     port: 4321,
     host: true,
-  }  
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
