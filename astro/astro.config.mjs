2c10326772936514ed3eb51e78d140c1f39cffbd
Rayfos <rayfos.fun@gmail.com>
2026-01-10 14:31:35 -0800

    npx astro add react

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rayfos.fun',
@@ -20,5 +22,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
