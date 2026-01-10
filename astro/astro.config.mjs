08cf2aed793a2ded1248a3093caca0945118b6dd
Rayfos <rayfos.fun@gmail.com>
2026-01-10 14:45:15 -0800

    npx astro add sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rayfos.fun',
@@ -24,5 +26,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
