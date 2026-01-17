debce2f16027fc49287dfb57ef220462929bd873
Rayfos <rayfos.fun@gmail.com>
2026-01-16 17:54:09 -0800

    upload 2048 to itch.io

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,11 +4,19 @@ import node from '@astrojs/node';
 import react from '@astrojs/react';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import singleFile from 'astro-single-file';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+const isItchBuild = process.env.ITCH_BUILD === 'true';
+
 // https://astro.build/config
 export default defineConfig({
+  outDir: isItchBuild ? './dist_itch' : './dist',
+  integrations: [
+    isItchBuild ? singleFile() : null
+  ],
+
   site: 'https://rayfos.fun',
   base: '/',
 
@@ -39,5 +47,11 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false,
     },
+  },
+
+  vite: {
+    build: {
+      assetsInlineLimit: isItchBuild ? 100000000 : 4096,
+    }
   },
 });
